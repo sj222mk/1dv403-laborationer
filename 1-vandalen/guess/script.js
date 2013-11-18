25ef5419b50275f6f75e9ec43fab57f665b7a558
sj222mk <sj222mk>
2013-11-19 00:31:10 +0100

    Guess klar

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,21 +2,37 @@
 
 window.onload = function(){
 	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
+	var secret = Math.floor(Math.random()*100)+1; // Slumptal mellan 1 - 100
+	var count = 0;
 	
-	// I denna funktion ska du skriva koden för att hantera "spelet"
+	// Varje gissning hanteras här
 	var guess = function(number){
-		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
-		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
-			
-		// Plats för förändring.
+		
+		var answers = [];
+		answers[0] = [true, "Grattis du vann! Det hemliga talet var " + secret + " och du behövde " + count + " gissningar för att hitta det."];
+		answers[1] = [false, "Det hemliga talet är högre!"];
+		answers[2] = [false, "Det hemliga talet är lägre!"];
+		answers[3] = [false, "Talet är utanför intervallet 0 - 100"];
+		answers[4] = [false, "Det du matade in tolkades inte som ett tal!"];
+        
+        if(isNaN(number)){
+            return (answers[4]);
+        }
+        else if(number < 1 || number > 100){
+			return (answers[3]);
+        }
+        
+        count ++;
 
-
-		// Returnera exempelvis: 
-		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
-		// [false, "Det hemliga talet är högre!"]
-		// [false, "Det hemliga talet är lägre!"]
-		// [false, "Talet är utanför intervallet 0 - 100"]		
+		if (number == secret){
+			return (answers[0]);
+		}
+		else if(number < secret){
+			return (answers[1]);
+		}
+		else if(number > secret){
+			return (answers[2]);
+		}
 	};
 	
 	// ------------------------------------------------------------------------------
