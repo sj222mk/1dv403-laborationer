49d1b417def8a7e06e659d20a8200b61ffd7c927
sj222mk <sj222mk>
2013-11-19 14:52:44 +0100

    Strängar klar

--- 1-vandalen/strings/script.js
+++ 1-vandalen/strings/script.js
@@ -1,43 +1,62 @@
 "use strict";
 
-window.onload = function(){
+window.onload = function () {
 
-	// I denna funktion ska du skriva koden för att hantera "spelet"
-	var convertString = function(str){
-		// Plats för förändring.		
-		// Returnera den konverterade strängen.
-		// Vid fel, kasta ett undantag med ett meddelande till användaren. 
-	
+    // Koden för att hantera "spelet"
+    var convertString = function (str) {
+
+        var message = ["Du måste skriva in en text för att kunna omvandla!"];
+        var i = 0;
+        var arr = [];
+
+        var trimmedStr = str.trim();
+        var length = trimmedStr.length;
+
+        if (length < 1) {
+            return message;
+        }
+
+        for (i = 0; i <= length; i++) {
+            var string = trimmedStr.charAt(i);
+
+            if (string === "a" || string === "A") {
+                string = "#";
+            }
+            else if (string === string.toLocaleUpperCase()) {
+                string = string.toLocaleLowerCase();
+            }
+            else if (string === string.toLocaleLowerCase()) {
+                string = string.toLocaleUpperCase();
+            }
+
+            arr[i] = [string];
+        }
+        arr[0] = arr.join("");
+        return arr[0];
+    };
+    // ------------------------------------------------------------------------------
 
 
+    // Kod för att hantera utskrift och inmatning. Denna ska du inte behöva förändra
+    var p = document.querySelector("#value"); // Referens till DOM-noden med id="#value"
+    var input = document.querySelector("#string");
+    var submit = document.querySelector("#send");
 
+    // Vi kopplar en eventhanterare till formulärets skickaknapp som kör en anonym funktion.
+    submit.addEventListener("click", function (e) {
+        e.preventDefault(); // Hindra formuläret från att skickas till servern. Vi hanterar allt på klienten.
 
+        p.classList.remove("error");
 
+        try {
+            var answer = convertString(input.value) // Läser in texten från textrutan och skickar till funktionen "convertString"
+            p.innerHTML = answer; 	// Skriver ut texten från arrayen som skapats i funktionen.	
+        } catch (error) {
+            p.classList.add("error"); // Växla CSS-klass, IE10+
+            p.innerHTML = error.message;
+        }
 
-	};
-	// ------------------------------------------------------------------------------
-
-
-	// Kod för att hantera utskrift och inmatning. Denna ska du inte behöva förändra
-	var p = document.querySelector("#value"); // Referens till DOM-noden med id="#value"
-	var input = document.querySelector("#string");
-	var submit = document.querySelector("#send");
-
-	// Vi kopplar en eventhanterare till formulärets skickaknapp som kör en anonym funktion.
-	submit.addEventListener("click", function(e){
-		e.preventDefault(); // Hindra formuläret från att skickas till servern. Vi hanterar allt på klienten.
-
-		p.classList.remove( "error");
-
-		try {
-			var answer = convertString(input.value) // Läser in texten från textrutan och skickar till funktionen "convertString"
-			p.innerHTML = answer;		// Skriver ut texten från arrayen som skapats i funktionen.	
-		} catch (error){
-			p.classList.add( "error"); // Växla CSS-klass, IE10+
-			p.innerHTML = error.message;
-		}
-	
-	});
+    });
 
 
 
