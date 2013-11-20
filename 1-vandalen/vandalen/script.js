43536a48b3ac5d33d388d0590c2af3fbaef7ffa0
sj222mk <sj222mk>
2013-11-21 00:08:16 +0100

    Vandalen 2

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,27 +1,45 @@
 "use strict";
 
 
-var makePerson = function(persArr){
-    function Person(name, age){
-    this.name = name;
-    this.age = Number(age);
-    }
-    
-    Person.prototype.Result = function (minAge, maxAge, averageAge, names){
-        
-        minAge = Math.min(this.age);
-        maxAge = Math.max(this.age);
-        
-        averageAge = (this.age).sum() / arguments.length;
-        
-        names = makePerson(name);
-        names.sort();
-        
-        return "Object {minAge: " + minAge + ", maxAge: " + maxAge + ", averageAge: " + averageAge + ", names: " + names;
-    };
-    
-    return result;
+var makePerson = function (persArr) {
+    var result = {};
+    persArr.person = ["name", "age"];
+
+    var names = [];
+    var minAge;
+    var maxAge;
+    var totalAge;
+
+    persArr.forEach(function (person) {
+        names.push(person.name);
+
+        minAge = (person.age).sort(function (a, b) {
+            return a.localeCompare(b).shift();
+        });
+
+        maxAge = (person.age).sort(function (a, b) {
+            return a.localeCompare(b).pop();
+        });
+
+        totalAge = (person.age).reduce(function (a, b) {
+            return a + b;
+        });
+    });
+
+    var averageAge = totalAge / persArr.length;
+
+    names.sort(function (a, b) {
+        return a.localeCompare(b);
+    });
+
+    names = names.join(", ");
+
+
+
+
+
+    return { minAge: minAge, maxAge: maxAge, averageAge: averageAge, names: names };
 };
-var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+var data = [{ name: "John Häggerud", age: 37 }, { name: "Johan Leitet", age: 36 }, { name: "Mats Loock", age: 46}];
 var result = makePerson(data);
 console.log(result);
