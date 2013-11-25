9be7c5cdce4871890363a1be2e34de581d2862fe
sj222mk <sj222mk>
2013-11-25 02:30:47 +0100

    Birthday

--- 1-vandalen/birthday/script.js
+++ 1-vandalen/birthday/script.js
@@ -1,51 +1,72 @@
 "use strict";
 
-window.onload = function(){
-
-	
-	var birthday = function(date){
-		
+window.onload = function () {
 
 
-			// Din kod här.
+    var birthday = function (date) {
+        try {
+            date == yyyy-MM--DD;
+        }
+        catch (ex) {
+            "Datumet är i fel format!"
+        }
+        var birthDay = new Date(date);
+        var today = new Date();
+        var bDay = new Date();
+        //bDay.setFullYear(bDay.getFullYear()); Ska man skriva ut det eller ej??
+        bDay.setMonth(birthDay.getMonth());
+        bDay.setDate(birthDay.getDate());
+        var days = (1000 * 60 * 60 * 24);
+
+        var difference = (bDay.getTime() - today.getTime());
+
+        if (difference === 0) {
+            return 0;
+        }
+        else if (difference < 0) {
+            bDay.setFullYear(today.getFullYear() + 1);
+            difference = (bDay.getTime() - today.getTime())
+        }
+        return Math.floor(difference / days);
 
 
 
 
-	};
-	// ------------------------------------------------------------------------------
+
+    };
+    // ------------------------------------------------------------------------------
 
 
-	// Kod för att hantera utskrift och inmatning. Denna ska du inte behöva förändra
-	var p = document.querySelector("#value"); // Referens till DOM-noden med id="#value"
-	var input = document.querySelector("#string");
-	var submit = document.querySelector("#send");
+    // Kod för att hantera utskrift och inmatning. Denna ska du inte behöva förändra
+    var p = document.querySelector("#value"); // Referens till DOM-noden med id="#value"
+    var input = document.querySelector("#string");
+    var submit = document.querySelector("#send");
 
-	// Vi kopplar en eventhanterare till formulärets skickaknapp som kör en anonym funktion.
-	submit.addEventListener("click", function(e){
-		e.preventDefault(); // Hindra formuläret från att skickas till servern. Vi hanterar allt på klienten.
+    // Vi kopplar en eventhanterare till formulärets skickaknapp som kör en anonym funktion.
+    submit.addEventListener("click", function (e) {
+        e.preventDefault(); // Hindra formuläret från att skickas till servern. Vi hanterar allt på klienten.
 
-		p.classList.remove( "error");
+        p.classList.remove("error");
 
-		try {
-			var answer = birthday(input.value) // Läser in texten från textrutan och skickar till funktionen "convertString"
-			var message;
-			switch (answer){
-				case 0: message = "Grattis på födelsedagen!";
-					break;
-				case 1: message = "Du fyller år imorgon!";
-					break;
-				default: message = "Du fyller år om " + answer + " dagar";
-					break;
-			}
+        try {
+            var answer = birthday(input.value) // Läser in texten från textrutan och skickar till funktionen "convertString"
+            var message;
+            switch (answer) {
+                case 0: message = "Grattis på födelsedagen!";
+                    break;
+                case 1: message = "Du fyller år imorgon!";
+                    break;
+                default: message = "Du fyller år om " + answer + " dagar";
+                    break;
+            }
 
-			p.innerHTML = message;
-		} catch (error){
-			p.classList.add( "error"); // Växla CSS-klass, IE10+
-			p.innerHTML = error.message;
-		}
-	
-	});
+            p.innerHTML = message;
+        } catch (error) {
+            p.classList.add("error"); // Växla CSS-klass, IE10+
+            p.innerHTML = error.message;
+        }
+
+    });
 
 
 
