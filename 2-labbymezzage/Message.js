22ef4bc4e834e36a8da7e16664dbdbb99842bf90
sj222mk <sj222mk>
2013-11-27 13:08:16 +0100

    LabbyStart

---
+++ 2-labbymezzage/Message.js
@@ -0,0 +1,32 @@
+﻿"use strict";
+//Konstruktorfunktion - klass för meddelanden
+function Message(message, date) {
+
+    this.getText = function () {
+        return message;
+    }
+    
+    this.setText = function(_text){
+        message = text;
+    }
+
+    this.getDate = function(){
+        return date;
+    }
+
+    this.setDate = function (_date) {
+        var currentDate = new Date();
+        date = (currentDate.setHours() + ":" + currentDate.setMinutes());
+
+    }
+}
+
+Message.prototype.toString = function () {
+    return this.getText() + " (" + this.getDate() + ")";
+}
+
+Message.prototype.getHTMLText = function () {
+    this.getText();
+    return this.setText();
+}
+
---
+++ 2-labbymezzage/script.js
@@ -0,0 +1,22 @@
+﻿"use strict";
+
+var mezz = {
+
+
+};
+
+window.onload = init();  
+
+function init(){
+    var mess = new Message("Testmeddelande", new Date());
+    alert(mess);
+    alert(mess.getText());
+    mess.setText("En annan test");
+    alert(mess);
+}
+
+window.onload = init();  
+
+//var p = document.querySelector("#value"); 
+//var input = document.querySelector("#string");
+//var submit = document.querySelector("#send");
