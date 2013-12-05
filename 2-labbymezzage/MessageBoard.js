2e4b39f9dd23cf7a7d9f8dd7648bd1070bf90fec
OpenShift guest <sj222mk@student.lnu.se>
2013-12-05 05:52:51 -0500

    Ta-bort knapp

--- 2-labbymezzage/MessageBoard.js
+++ 2-labbymezzage/MessageBoard.js
@@ -40,6 +40,14 @@ messageSystem.MessageBoard = {
         messageSystem.MessageBoard.renderMessages();
         };
     
+    /*messageSystem.showTime = function(){
+        alert("Visa tid");
+    }
+    */
+    messageSystem.removeMessage = function(){
+      alert("är du säker?");  
+    };
+    
     messageSystem.messageCount = function(){
         var count = document.createElement("p");
         var number = messageSystem.MessageBoard.messages.length;
@@ -53,10 +61,37 @@ messageSystem.MessageBoard = {
         var div = document.createElement("div");
         div.classname = "board";
         
+        //Lägger til text i meddelande
         var text = document.createElement("p");
         text.innerHTML = messageSystem.MessageBoard.messages[messageID].getHTMLText();
         div.appendChild(text);
         
+        //Lägger till tid-knapp i meddelande
+        /*var imgTime = document.createElement("img");
+        imgTime.href = "#";
+        imgTime.src = "pics/clock_green.png";
+        imgTime.class = "messageButtons";
+        imgTime.alt = "Tid-knapp";
+        
+        imgTime.onclick =  function(){
+            messageSystem.messageBoard.showTime(messageID);
+        };
+        div.appendChild(imgTime);*/
+        
+        
+        //Lägger till delete-knapp i meddelande
+        var imgClose = document.createElement("img");
+        imgClose.href = "#";
+        imgClose.src = "pics/Delete16.png";
+        imgClose.class = "messageButtons";
+        imgClose.alt = "Ta bort-knapp";
+        
+        imgClose.onclick =  function(){
+            messageSystem.messageBoard.removeMessage(messageID);
+        };
+        div.appendChild(imgClose);
+        
+        //Lägger till tid i meddelande
         var time = document.createElement("p");
         time.innerHTML = messageSystem.MessageBoard.messages[messageID].getDate();
         div.appendChild(time);
