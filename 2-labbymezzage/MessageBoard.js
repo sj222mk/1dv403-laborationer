3f8dc9925b8e4a0b616d2f6ae2a2730e34291c30
OpenShift guest <sj222mk@student.lnu.se>
2013-12-08 14:20:24 -0500

    labby 7 finjusterad

--- 2-labbymezzage/MessageBoard.js
+++ 2-labbymezzage/MessageBoard.js
@@ -33,10 +33,6 @@ messageSystem.MessageBoard = {
             }
         }; 
         messageSystem.messageCount();
-        
-        /*document.getElementById("closeButton").onclick = function() {
-            messageSystem.messageBoard.removeMessage(messageID);
-        }*/
     };
     
     messageSystem.addMessage = function (text) {
@@ -64,28 +60,10 @@ messageSystem.MessageBoard = {
         var div = document.createElement("div");
         div.classname = "board";
         
-        //Lägger til text i meddelande
-        var text = document.createElement("p");
-        text.innerHTML = messageSystem.MessageBoard.messages[messageID].getHTMLText();
-        div.appendChild(text);
-        
-        //Lägger till tid-knapp i meddelande
-        /*var imgTime = document.createElement("img");
-        imgTime.href = "#";
-        imgTime.src = "pics/clock_green.png";
-        imgTime.class = "messageButtons";
-        imgTime.alt = "Tid-knapp";
-        
-        imgTime.onclick =  function(){
-            messageSystem.messageBoard.showTime(messageID);
-        };
-        div.appendChild(imgTime);*/
-        
-        
         //Lägger till delete-knapp i meddelande
         var imgClose = document.createElement("img");
         imgClose.href = "#";
-        imgClose.src = "pics/Delete16.png";
+        imgClose.src = "pics/remove24.png";
         imgClose.class = "messageButtons";
         imgClose.alt = "Ta bort-knapp";
         
@@ -93,6 +71,23 @@ messageSystem.MessageBoard = {
             messageSystem.removeMessage(messageID);
         };
         div.appendChild(imgClose);
+        
+        //Lägger till tid-knapp i meddelande
+        var imgTime = document.createElement("img");
+        imgTime.href = "#";
+        imgTime.src = "pics/clock_green24.png";
+        imgTime.class = "messageButtons";
+        imgTime.alt = "Tid-knapp";
+        
+        imgTime.onclick =  function(){
+            messageSystem.messageBoard.showTime(messageID);
+        };
+        div.appendChild(imgTime);
+        
+        //Lägger til text i meddelande
+        var text = document.createElement("p");
+        text.innerHTML = messageSystem.MessageBoard.messages[messageID].getHTMLText();
+        div.appendChild(text);
         
         //Lägger till tid i meddelande
         var time = document.createElement("p");
