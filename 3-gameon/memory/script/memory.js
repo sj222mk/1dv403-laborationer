f92ade89ccad33a4753c2c33a208b86e2197fd96
OpenShift guest <sj222mk@student.lnu.se>
2013-12-09 15:25:34 -0500

    Labby mezzage validerad

---
+++ 3-gameon/memory/script/memory.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var memoryGame = memoryGame || {};
+
+memoryGame.Memory = {
+
+   
+};
+
+    memoryGame.init = function(){
+        onload.event();
+       
+        }; 
+        
