d311b7021ec151d5939a36bceeb8625a27689158
OpenShift guest <sj222mk@student.lnu.se>
2013-12-09 19:39:57 -0500

    Memory 7

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -4,15 +4,47 @@ var memoryGame = memoryGame || {};
 
 memoryGame.Memory = {
     random : [],
-   
+    
 };
 
     memoryGame.init = function(e){
-        var newRandom = new memoryGame.RandomGenerator.getPictureArray(2, 4);
-        memoryGame.Memory.random.push(newRandom);
-        alert(memoryGame.Memory.random);
-       
+        var row = 3;
+        var col = 4;
+        
+        memoryGame.RandomGenerator.getPictureArray(row, col).forEach(function(number){
+            memoryGame.Memory.random.push(number); 
+        });
+        
+        memoryGame.makeTable(row, col);
     }; 
+    
+    memoryGame.makeTable = function(rows, cols){
+        var no = 0;
+        
+        var table = document.createElement("table");
+        var tbody = document.createElement("tbody"); 
+        table.appendChild(tbody);
+        
+      
+        /*tbody.insertRow(0);
+        tbody.rows[0].insertCell(0);
+        tbody.rows[0].cells[0].appendChild(document.create("Cell 1,1"));
+        tbody.rows[0].insertCell(1);
+        tbody.rows[0].cells[1].appendChild(document.createTextNode("Cell 2,1"));*/
+        
+        for(var r = 0; r < rows; r++){
+            tbody.insertRow(r);
+            
+            for(var c = 0; c < cols; c++){
+                tbody.rows[r].insertCell(c);
+                var img = document.createElement("img");
+                img.src = "pics/" + memoryGame.Memory.random[no] + ".png";
+                tbody.rows[r].cells[c].appendChild(img);
+                no += 1;
+            }
+        }
+        document.body.appendChild(table);
+    };
 
 window.onload = function () {
    memoryGame.init();
