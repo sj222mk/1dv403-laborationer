3c2e7f8d96b5df2268cd01b22ae7cf9f078b6605
OpenShift guest <sj222mk@student.lnu.se>
2014-01-05 15:15:06 -0500

    Memory x3

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -2,48 +2,42 @@
 
 var memoryGame = memoryGame || {};
 
-memoryGame.counterDraw = 0;
-memoryGame.counterAlike = 0;
-memoryGame.picOne;
-memoryGame.picTwo;
-
-memoryGame.Memory = {
-    random : [],
-
-
-
-    init : function(){
-        var col = 4;
-        var row = 3;
-        /*memoryGame.id = node;*/
-        memoryGame.RandomGenerator.getPictureArray(row, col).forEach(function(number){
-            memoryGame.Memory.random.push(number); 
-        });
+memoryGame.Memory = function(rows, cols, memoryId, memoryName){
+    var counterDraw = 0;
+    var counterAlike = 0;
+    var picOne;
+    var picTwo;
+    var col = cols;
+    var row = rows;
+    var id = memoryId;
+    var name = memoryName;
+    var random = [];
+    
+    memoryGame.RandomGenerator.getPictureArray(row, col).forEach(function(number){
+        random.push(number); 
+    });
+    
+    makeTable(row, col, id, name);
         
-        memoryGame.makeTable(row, col);
-        
-        
-    } 
-};    
-    memoryGame.makeTable = function(rows, cols){
+    function makeTable(row, col, id, name){
         var no = 0;
         
         var table = document.createElement("table");
         var thead = document.createElement("thead");
         
         var th = document.createElement("th");
-        th.colSpan = cols;
-        th.innerHTML = "Spel 1";
+        th.colSpan = col;
+        th.innerHTML = name;
         thead.appendChild(th);
         table.appendChild(thead);
         var tbody = document.createElement("tbody"); 
         table.appendChild(tbody);
         
         
-        for(var r = 0; r < rows; r++){
+        for(var r = 0; r < row; r++){
             tbody.insertRow(r);
             
-            for(var c = 0; c < cols; c++){
+            for(var c = 0; c < col; c++){
                 tbody.rows[r].insertCell(c);
                 var a = document.createElement("a");
                 a.href = "#";
@@ -55,7 +49,7 @@ memoryGame.Memory = {
                 img.name = no;
                 
                 a.onclick = function(){
-                    memoryGame.turnAround(this);
+                    turnAround(this);
                 };
                 
                 a.appendChild(img);
@@ -64,46 +58,47 @@ memoryGame.Memory = {
                 no += 1;
             }
         }
-        var memory = document.getElementById("memory1");
-        memory.appendChild(table);
-    };
+        var memoryNr = document.getElementById(id);
+        memoryNr.appendChild(table);
+    }
     
-    memoryGame.lookAlike = function(){
-        if (memoryGame.picOne.children[0].src !== memoryGame.picTwo.children[0].src){
+    function lookAlike(){
+        if (picOne.children[0].src !== picTwo.children[0].src){
             setTimeout(function(){
-                memoryGame.turnBack(); 
+                turnBack(); 
                 }, 1000);
             }
         else{
-            memoryGame.counterAlike +=1;
+            counterAlike +=1;
             
-            if(memoryGame.counterAlike === memoryGame.col * memoryGame.row / 2){
+            if(counterAlike === col * row / 2){
                 setTimeout(function(){
-                    alert("Spelet är slut! Du klarade det på " + memoryGame.counterDraw/2 + " försök!");
+                    alert(id + " är slut! Du klarade det på " + counterDraw/2 + " försök!");
                 }, 1000);
             }
         }
-    };
+    }
     
-    memoryGame.turnAround = function(a){
-        memoryGame.counterDraw +=1;
+    function turnAround(a){
+        counterDraw +=1;
         
-        if (memoryGame.counterDraw % 2 !== 0){        
-            memoryGame.picOne = a;
-            memoryGame.picOne.children[0].src = "pics/" + memoryGame.Memory.random[memoryGame.picOne.id] + ".png";
+        if (counterDraw % 2 !== 0){        
+            picOne = a;
+            picOne.children[0].src = "pics/" + random[picOne.id] + ".png";
         }
         else{
-            memoryGame.picTwo = a;
-            memoryGame.picTwo.children[0].src = "pics/" + memoryGame.Memory.random[memoryGame.picTwo.id] + ".png";
-            memoryGame.lookAlike();
+            picTwo = a;
+            picTwo.children[0].src = "pics/" + random[picTwo.id] + ".png";
+            lookAlike();
         }
-    };
+    }
     
-    memoryGame.turnBack = function(){
-        memoryGame.picOne.children[0].src = "pics/0.png";
-        memoryGame.picTwo.children[0].src = "pics/0.png";
-    };
+    function turnBack(){
+        picOne.children[0].src = "pics/0.png";
+        picTwo.children[0].src = "pics/0.png";
+    }
 
-window.onload = function () {
-   memoryGame.Memory.init();
-};
+
+}; 
+
+
--- 3-gameon/memory/script/memoryapp.js
+++ 3-gameon/memory/script/memoryapp.js
@@ -4,9 +4,12 @@ var memoryGame = memoryGame || {};
 
 memoryGame.MemoryApp = {
    init: function(){
-      var memory1 = new memoryGame.Memory(3, 4, "memory1");
-      /*var memory2 = new memoryGame.Memory(3, 4, "memory2");*/
-      memory1.start();
+      var memory1 = new memoryGame.Memory(4, 2, "memory1", "Memory 1");
+      var memory2 = new memoryGame.Memory(4, 3, "memory2", "Memory 2");
+      var memory3 = new memoryGame.Memory(4, 4, "memory3", "Memory 3");
+      //memory1.memoryGame.makeTable();
       /*memory2.start();*/
    }
 };
+
+window.onload = memoryGame.MemoryApp.init;     
