91202f5f85e2428215c4cf2a5d0317ab851f2948
OpenShift guest <sj222mk@student.lnu.se>
2014-01-07 19:30:28 -0500

    variabler o regExp klart

--- 4-forms/script/script.js
+++ 4-forms/script/script.js
@@ -3,13 +3,34 @@
 var validation = validation || {};
 
 validation.Form = function(){
-    var ;
-    var random = [];
+    var form = document.getElementById("form");
+    var name = document.getElementById("name");
+    var pnr = document.getElementById("pnr");
+    var email = document.getElementById("email");
+    var price = document.getElementById("price");
     
-    init = function(){
+    //Ej tal
+    var regExpName = //;
+    
+    //5 Siffror
+    var regExpPnr = //;
+    
+    //Email-form
+    var regExpEmail = //;
+    
+    //Ett valt alternativ
+    var regExpPrice = "price.value("low").checked = true || price.value("middle").checked = true ||price.value("high").checked = true;"
+    
+    var valid = function(){
         
+        var first = form.elements[0];
+        first.select();
+     
+     
+     
+    };   
 };
 
 window.onload = function () {
-   validation.Form.init();
+   validation.Form.valid();
 };  
