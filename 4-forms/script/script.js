e8a33e0401fae7662c55d26cf5d17e44f2f527e6
OpenShift guest <sj222mk@student.lnu.se>
2014-01-08 17:32:54 -0500

    lite kode

--- 4-forms/script/script.js
+++ 4-forms/script/script.js
@@ -3,11 +3,10 @@
 var validation = validation || {};
 
 validation.Form = function(){
-    var form = document.getElementById("form");
-    var fname = document.getElementById("fname");
-    var lname = document.getElementById("lname");
-    var pnr = document.getElementById("pnr");
-    var email = document.getElementById("email");
+    var formOne = document.forms[0];
+    //var names = document.getElementByClassName["name"];
+    var pNr = document.getElementById("pnr");
+    //var eMails = document.getElementByClassName("email");
     var price = document.getElementById("price");
     
     //Ej tal
@@ -15,7 +14,7 @@ validation.Form = function(){
     
     //5 Siffror i posnummerform
     var regExpPnr = /(SE)? ?[1-9]{1}\d{2}\-? ?\d{2}$/;
-    var regExpPnrRepPat = /^SE| |\-|$/;
+    var regExpPnrRepPat = /^(SE)|( )|(\-)|/g;
     
     //Email-form
     var regExpEmail = /^(?!\.)(w|-|\.|#){1,64}(?!\.)@(?!\.)[-.a-zåäö0-9]{4,253}/;
@@ -23,24 +22,65 @@ validation.Form = function(){
     //Ett valt alternativ
     var regExpPrice = "price.value('low').checked = true || price.value('middle').checked = true ||price.value('high').checked = true";
     
-    valid();
+    //Felmeddelanden
+    var blank = "Detta fält får inte lämnas blankt";
+    var noChoice = "Du måste välja ett alternativ";
+    var notName = "Nmnet får inte innehålla några siffror";
+    var notPnr = "Postnumret är felaktigt";
+    var notEmail = "Eposten är felaktig";
     
-    function valid(){
-        
-        fname.onfocus = function(){
+    /*formOne.elements[0].focus();
+    for(var i = 0; i < formOne.Lenght; i++){
+        formOne[1].onfocus = function(){
         this.select();
+        };
+    }*/
+    
+    pNr.onblur = function(e){
+        var input = this.value;
+        if(regExpPnr.test(input)){
+            input = input.replace(regExpPnrRepPat, "");
+            this.value = input;
+            this.setAttribute("class", "ok");
         }
+        else{
+            this.setAttribute("class", "notValid");
+            if(input === undefined){
+                setMessage(e, blank);
+            }
+            else{
+                setMessage(pNr, notPnr);
+            }
+        }
+    };
+    
+    //Infogar felmeddelande
+    function setMessage(node, text){
+        var error = document.createTextNode(text);
+        node.appendChild(error);
+    }   
+        
+    
+    
         
         
-        pnr.onfocus = function(){
+        
+        
+       // pnr.onfocus = function(){
              
                 //if(regExpPnr.test(this.value)){
-                    alert("Rätt!");
+                    //alert("Rätt!");
                 //}
-        };   
+        //};   
         
-    };
+    
+    
+
+    
+    
+    
 };
+
 window.onload = function () {
-   validation.Form;
+   validation.Form();
 };  
