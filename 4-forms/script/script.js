4c13db656201f27adc83e784f22f21c1f935619a
OpenShift guest <sj222mk@student.lnu.se>
2014-01-10 15:54:39 -0500

    lite mer kod

--- 4-forms/script/script.js
+++ 4-forms/script/script.js
@@ -4,6 +4,7 @@ var validation = validation || {};
 
 validation.Form = function(){
     var formOne = document.getElementsByClassName("test");
+    var subButton = document.getElementById("submitButton");
     
     //Ej tal
     var regExpName = /^\D+$/;
@@ -16,7 +17,7 @@ validation.Form = function(){
     var regExpEmail = /^(?!\.)(\w|-|\.|#){1,64}(?!\.)@(?!\.)[-.a-zåäö0-9]{4,253}/;
     
     //Ett valt alternativ
-    var checkSelect = "price.value('def').checked = true";
+    //var checkSelect = "price.value('def').checked ===;
     
     //Felmeddelanden
     var blank = "Detta fält får inte lämnas tomt";
@@ -29,34 +30,45 @@ validation.Form = function(){
     
     formOne[0].focus();
     //Select funkar ej!!!
-    formOne[0].select();
+    /*formOne[0].select();
     for(i = 0; i < formOne.length; i++){
         formOne[i].onfocus = function(){
-        this.input.select();
+        this.select();
         };
-    }
+    }*/
     
-    for(i = 0; i < formOne.length; i++){
-        formOne[i].onblur = function(){
-            //alert(this.id);
-            switch(true){
-                case this.id === "lname" || this.id === "fname":
-                    valid(this,regExpName, notName);
+    subButton.disabled = true;
+    checkAll();
+    
+    
+    
+    function checkAll(){
+        for(i = 0; i < formOne.length; i++){
+            formOne[i].onchange = function(){
+                checkOne(this);
+                checkReady();
+                };
+            }
+        }
+    
+    function checkOne(inp){
+        switch(true){
+                case inp.id === "lname" || inp.id === "fname":
+                    valid(inp,regExpName, notName);
                     break;
-                case this.id === "pnr":
-                    valid(this,regExpPnr, notPnr);
+                case inp.id === "pnr":
+                    valid(inp,regExpPnr, notPnr);
                     break;
-                case this.id === "email":
-                    valid(this,regExpEmail, notEmail);
+                case inp.id === "email":
+                    valid(inp,regExpEmail, notEmail);
                     break;
-                case this.id === "price":
-                    validSelect(this);
+                case inp.id === "price":
+                    validSelect(inp);
                     break;
                 default:
                     break;
             }
-        };
-    }
+        }
     
     //Validering med regExp
     function valid(node, reg, mess){
@@ -64,33 +76,35 @@ validation.Form = function(){
         if(reg.test(input)){
             if(node.id === "pnr"){
                 setPnr(node);
-            }
+                }
             removeErrorMessage(node);
-        }
+            node.setAttribute("class", "ok");
+            }
         else{
             if(input === ""){
                 setErrorMessage(node, blank);
-            }
+                }
             else{
                 setErrorMessage(node, mess);
+                }
             }
         }
-    }
     
     //Validering av listobjekt
     function validSelect(node){
-        if(checkSelect){
-            removeErrorMessage(node);   
-        }
+        if(node.value !== "def"){
+            removeErrorMessage(node); 
+            node.setAttribute("class", "ok");
+            }
         else{
             setErrorMessage(node, noChoice);
-        }
-   } 
+            }
+        } 
     
     //Godkänd validering
     function setPnr(node){
         node.value = node.value.replace(regExpPnrRepPat, "");
-    }
+        }
     
     //Infogar felmeddelande
     function setErrorMessage(node, text){
@@ -102,28 +116,56 @@ validation.Form = function(){
         if(node.className === "notValid"){
             var oldT = document.getElementById("errorTag");
             oldT.parentNode.replaceChild(newT, oldT);
-        }
-        else
-        {
+            }
+        else{
             node.setAttribute("class", "notValid");
             node.parentNode.appendChild(newT);
-        }
+            }
         node.focus();
-    }   
+        }   
     
     //Tar bort felmeddelande
     function removeErrorMessage(node){
         var removeT = document.getElementById("errorTag");
-        if(removeT.parentNode){
+        if(removeT !== null){
             removeT.parentNode.removeChild(removeT);
+            }
         }
-        node.setAttribute("class", "ok");
+    
+    function checkReady(){
+        var count = 0;
+        for(i = 0; i < formOne.length; i++){
+            if(formOne[i].class === "test"){
+                count +=1;
+                }
+            }
+        if(count === formOne.length){
+                subButton.disabled = false;
+            }
+        }
+        
+function checkForm(){
+        var error = false;
+        for(i = 0; i < formOne.length; i++){
+            checkOne(formOne[i]);
+            if(formOne[i].class !== "ok"){
+                error = true;
+                }
+            else{
+                output(error);        
+            }
+            return error;
+        }
     }
-    
-    
-    
-    
-    
+     
+    function output(error){
+        var fValues = [];
+        for(var element in formOne){
+            fValues.push(element.label.value + ": " + element.value);
+            }
+        error.confirm(fValues);
+        return error;
+        }
 };
 
 window.onload = function () {
