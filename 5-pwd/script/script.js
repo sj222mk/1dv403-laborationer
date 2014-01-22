48c280da49b178cd1cbef1d3e4c0c9a5665ce069
OpenShift guest <sj222mk@student.lnu.se>
2014-01-22 17:40:29 -0500

    Layout o fönster klart

---
+++ 5-pwd/script/script.js
@@ -0,0 +1,79 @@
+"use strict";
+
+var DESKTOP = DESKTOP || {};
+
+DESKTOP.View = function(){
+    var content = document.getElementById("content");
+    var miniWin;
+    
+    createMiniWin();
+    
+    function createMiniWin(){
+        var footer = document.getElementById("footer");
+        miniWin = document.createElement("img");
+        miniWin.id = "miniWindow";
+        miniWin.class = "closed";
+        miniWin.href = "#";
+        miniWin.src = "pics/photo_folder.png";
+        footer.appendChild(miniWin);
+        
+        miniWin.onclick =  function(){
+                if(miniWin.class === "closed"){
+                    miniWin.class = "open";
+                    createWindowApp();
+                }
+            };
+        }
+    
+    function createWindowApp(){
+        var windowApp = document.createElement("div");
+        windowApp.id = "app";
+        content.appendChild(windowApp);
+        
+        var winHeader = document.createElement("div");
+        winHeader.id = "windowHeader";
+        var h2 = document.createElement("h2");
+        var header = document.createTextNode("Image Viewer");
+        h2.appendChild(header);
+        winHeader.appendChild(h2);
+        windowApp.appendChild(winHeader);
+        
+        //Stänga-knapp i hörnet
+        var closeButton = document.createElement("img");
+        closeButton.id = "closeButton";
+        closeButton.href = "#";
+        closeButton.src = "pics/cancel.png";
+        winHeader.appendChild(closeButton);
+        
+        var winBody = document.createElement("div");
+        winBody.id = "windowBody";
+        windowApp.appendChild(winBody);
+        
+        var winFooter = document.createElement("div");
+        winFooter.id = "windowFooter";
+        windowApp.appendChild(winFooter);
+        
+        closeButton.onclick =  function(){
+            closeWindowApp(windowApp);
+            };
+        }
+    
+    function closeWindowApp(windowApp){
+        //var winApp = document.getElementById("app");
+        windowApp.parentNode.removeChild(windowApp);
+        miniWin.class = "closed";
+    }
+};
+
+window.onload = function () {
+   DESKTOP.View();
+};  
+
+//Lägger till delete-knapp i meddelande
+        /*var imgClose = document.createElement("img");
+        imgClose.href = "#";
+        imgClose.src = "pics/remove24.png";
+        imgClose.class = "messageButtons";
+        imgClose.alt = "Ta bort-knapp";*/
+        
+        
